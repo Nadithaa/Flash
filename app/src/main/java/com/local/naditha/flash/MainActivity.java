19b02098076c086a96bbd605812493ff047be1d8
Naditha Gallella <naditha100@gmail.com>
2017-11-24 22:43:15 +0530

    adding interfaces to flashlight app

--- app/src/main/java/com/local/naditha/flash/MainActivity.java
+++ app/src/main/java/com/local/naditha/flash/MainActivity.java
@@ -1,13 +1,77 @@
 package com.local.naditha.flash;
 
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.graphics.Camera;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+    ImageButton imageButton;
+   android.hardware.Camera camera;
+    android.hardware.Camera.Parameters parameters;
+
+    boolean isflash = false;
+    boolean isOn = false;
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageButton = (ImageButton)findViewById(R.id.imageButton);
+
+        if(getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH))
+        {
+            camera = android.hardware.Camera.open();
+            parameters = camera.getParameters();
+
+            isflash = true;
+
+        }
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            if(isflash){
+
+
+            }
+            else {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Error...!");
+                builder.setMessage("Flash is not available in this device ");
+                builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        finish();
+                    }
+                });
+                AlertDialog alertDialog = builder.create();
+                        alertDialog.show();
+
+
+            }
+
+
+
+            }
+        });
+
+
+
+
+
+
+
     }
 }
